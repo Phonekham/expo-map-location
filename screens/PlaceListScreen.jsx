1ce907d92fc80ef086e8af1f09704c08fc62fa79
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-07-14 12:13:25 +0700

    output list of places

--- screens/PlaceListScreen.jsx
+++ screens/PlaceListScreen.jsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { StyleSheet, Text, View, Platform } from "react-native";
+import { FlatList, StyleSheet, Text, Platform } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import HeaderButton from "../components/HeaderButton";
+import { useSelector } from "react-redux";
 
-const PlaceListScreen = () => {
+import HeaderButton from "../components/HeaderButton";
+import PlaceItem from "../components/PlaceItem";
+
+const PlaceListScreen = (props) => {
+  const places = useSelector((state) => state.places.places);
+
   return (
-    <View>
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <PlaceItem
+          image={null}
+          title={itemData.item.title}
+          onSelect={() => {
+            props.navigation.navigate("PlaceDetail", {
+              placeTitle: itemData.item.title,
+              placeId: itemData.item.id,
+            });
+          }}
+        ></PlaceItem>
+      )}
+    >
       <Text>place i love you</Text>
-    </View>
+    </FlatList>
   );
 };
 
