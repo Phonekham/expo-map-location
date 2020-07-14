22d5b2ff8b9b97b59da7f76bbc5ed5bc64f3602d
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-07-14 08:28:50 +0700

    navigation setup

---
+++ screens/PlaceListScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const PlaceListScreen = () => {
+  return (
+    <View>
+      <Text>plaaces i love you</Text>
+    </View>
+  );
+};
+
+export default PlaceListScreen;
+
+const styles = StyleSheet.create({});
